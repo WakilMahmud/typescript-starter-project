f59445e6dbb3cdb49643591c87621b27d55afec5
WakilMahmud <wakilmahmud30@gmail.com>
2023-11-22 03:09:20 +0600

    feat: Create student and get student

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,13 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application } from 'express';
 const app: Application = express();
 import cors from 'cors';
+import { StudentRoutes } from './app/modules/student/student.route';
 
 //parsers
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-  const a = 10;
-  res.send(a);
-});
+// application routes
+app.use('/api/v1/students', StudentRoutes);
 
 export default app;
